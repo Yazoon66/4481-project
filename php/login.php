1a15a61ed0be12a8536252e87eb01b876a2faa8f
Bailey Luu <50964948+BaileyLuu@users.noreply.github.com>
2023-04-03 00:38:03 -0400

    added nonce verification and password_verify() and prepared statements
    password_verify() which is a php built-in function; to verify the hash

--- php/login.php
+++ php/login.php
@@ -3,16 +3,26 @@ session_start(); // Start a new or resume an existing session
 
 include_once "config.php"; // Include the configuration file that sets up a connection to the database
 
-$email = mysqli_real_escape_string($conn, $_POST['email']); // Sanitize and store the email submitted via POST
-$password = mysqli_real_escape_string($conn, $_POST['password']); // Sanitize and store the password submitted via POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['nonce']) && $_POST['nonce'] === $_SESSION['nonce']) {
+        if (isset($_POST["email"]) && isset($_POST['password'])) {
+            $email = mysqli_real_escape_string($conn, $_POST['email']); // Sanitize and store the email submitted via POST
+            $password = mysqli_real_escape_string($conn, $_POST['password']); // Sanitize and store the password submitted via POST
+        }
+    } else {
+        echo "CSRF Attack!";
+    }
+}
 
-if(!empty($email) && !empty($password)){ // If email and password are not empty
-    $sql = mysqli_query($conn, "SELECT * FROM users WHERE email = '{$email}'"); // Query the database to find the user with the submitted email
-    if(mysqli_num_rows($sql) > 0){ // If the query returns at least one row
-        $row = mysqli_fetch_assoc($sql); // Fetch the result as an associative array
-        $user_pass = md5($password); // Hash the submitted password using md5
+if (!empty($email) && !empty($password)) { // If email and password are not empty
+    $stmt = $conn->prepare("SELECT * FROM users WHERE email = ?"); // Query the database to find the user with the submitted email
+    $stmt->bind_param("s", $email); // The bind_param() method automatically escapes the user data and prevents SQL injection.
+    $stmt->execute();
+    $result = $stmt->get_result();
+    if($result->num_rows > 0){ // If the query returns at least one row
+        $row = $result->fetch_assoc(); // Fetch the result as an associative array
         $enc_pass = $row['password']; // Get the hashed password stored in the database for the user
-        if($user_pass === $enc_pass){ // If the hashed password from the form matches the hashed password from the database
+        if (password_verify($password, $enc_pass)) { // If the hashed password from the form matches the hashed password from the database
             $status = "Active now"; // Set the user's status to "Active now"
             $sql2 = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id = {$row['unique_id']}"); // Update the user's status in the database
             if($sql2){ // If the query was successful
